99b4759e8f19856a2e0c2b99b0489dbe7f9a9c95
elphizu <suryakantupadhyay18@gmail.com>
2026-09-04 19:08:58 +0530

    test: split sample test into unit and integration suites

---
+++ tests/Integration/SampleTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Integration test scaffold.
+ *
+ * @package PressAgent
+ */
+
+namespace PressAgent\Tests\Integration;
+
+/**
+ * Placeholder integration test case.
+ */
+class SampleTest extends \WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		$this->assertTrue( true );
+	}
+}
